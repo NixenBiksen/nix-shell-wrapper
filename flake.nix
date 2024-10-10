71d9ada0e7a5281a80d86c117a43bd962f23d122
Tethys Svensson <freaken@freaken.dk>
2024-10-10 14:32:12 +0200

    Some cleanup

--- flake.nix
+++ flake.nix
@@ -2,24 +2,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
-    systems.url = "github:nix-systems/default";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
-      systems,
+      flake-utils
     }:
-    flake-utils.lib.eachSystem (import systems) (
+    flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        packages = flake-utils.lib.flattenTree {
+        packages = rec {
           nix-shell-wrapper = pkgs.callPackage ./derivation.nix { inherit system; };
+          default = nix-shell-wrapper;
         };
       }
     );
