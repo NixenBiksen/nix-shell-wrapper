07cc9b5a72151fe703457f920b16231d9cad062e
Tethys Svensson <freaken@freaken.dk>
2024-10-19 20:45:57 +0200

    Add support for additional flakes in expressions

--- src/main.rs
+++ src/main.rs
@@ -115,8 +115,24 @@ struct DerivationCommand {
 
 #[derive(Parser, Debug)]
 struct ExprsCommand {
+    /// Extra flake paths to include. Use the form `--flake=flakeName=PATH`
+    #[arg(long, value_parser=parse_flake_pair)]
+    flake: Vec<(String, PathBuf)>,
     /// A number of nix expressions; each expression must evaluate to a package
     exprs: Vec<String>,
+}
+
+fn parse_flake_pair(s: &str) -> Result<(String, PathBuf), String> {
+    if let Some((left, right)) = s.split_once('=') {
+        Ok((
+            left.to_string(),
+            PathBuf::from(right)
+                .canonicalize()
+                .map_err(|e| format!("Cannot canonicalize path {right:?}: {e:?}"))?,
+        ))
+    } else {
+        Err(format!("Cannot parse {s:?} as a flake+path pair"))
+    }
 }
 
 fn main() -> Result<()> {
@@ -142,7 +158,7 @@ fn main() -> Result<()> {
             cmd = std::process::Command::new("nix");
             cmd.args(["shell", "--impure", "--expr"]);
 
-            let mut combined_expr = expr_prefix();
+            let mut combined_expr = expr_prefix(Vec::new());
             combined_expr.push_str("(callPackage ");
             combined_expr.push_str(&derivation);
             combined_expr.push(' ');
@@ -151,21 +167,11 @@ fn main() -> Result<()> {
             cmd.arg(combined_expr);
             env.push(truncate_string(&derivation));
         }
-        App::Exprs(ExprsCommand { exprs }) | App::ExprsExternal(exprs) => {
-            cmd = std::process::Command::new("nix");
-            cmd.args(["shell", "--impure", "--expr"]);
-
-            let mut combined_expr = expr_prefix();
-
-            for expr in exprs {
-                combined_expr.push('(');
-                combined_expr.push_str(&expr);
-                combined_expr.push_str(") ");
-                env.push(truncate_string(&expr));
-            }
-
-            combined_expr.push(']');
-            cmd.arg(combined_expr);
+        App::Exprs(ExprsCommand { flake, exprs }) => {
+            cmd = make_exprs_command(exprs, flake, &mut env)
+        }
+        App::ExprsExternal(exprs) => {
+            cmd = make_exprs_command(exprs, Vec::new(), &mut env);
         }
     }
     let env = format!("{}", env.join("+"));
@@ -181,27 +187,58 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn expr_prefix() -> String {
-    let combined_expr;
-    if let Ok(flake) = std::env::var("NIX_SHELL_WRAPPER_FLAKE") {
-        combined_expr = format!(
-            r#"
-                    let
-                        systemFlake = builtins.getFlake {flake:?};
-                    in
-                        with systemFlake.nix-shell-wrapper-pkgs.{NIX_SHELL_WRAPPER_SYSTEM:?}.default; [
-                "#
-        );
-    } else {
-        combined_expr = format!(
-            r#"
-                    let
-                        nixpkgs = builtins.getFlake "nixpkgs";
-                        pkgs = import nixpkgs {{ system = {NIX_SHELL_WRAPPER_SYSTEM:?}; }};
-                    in
-                        with pkgs; [
-                "#
-        );
+fn make_exprs_command(
+    exprs: Vec<String>,
+    extra_flakes: Vec<(String, PathBuf)>,
+    env: &mut Vec<String>,
+) -> std::process::Command {
+    let mut cmd = std::process::Command::new("nix");
+    cmd.args(["shell", "--impure", "--expr"]);
+
+    let mut combined_expr = expr_prefix(extra_flakes);
+
+    for expr in exprs {
+        combined_expr.push('(');
+        combined_expr.push_str(&expr);
+        combined_expr.push_str(") ");
+        env.push(truncate_string(&expr));
     }
-    combined_expr
+
+    combined_expr.push(']');
+    cmd.arg(combined_expr);
+    cmd
+}
+
+fn expr_prefix(extra_flakes: Vec<(String, PathBuf)>) -> String {
+    let mut let_exprs = Vec::new();
+    let mut with_exprs = Vec::new();
+
+    if let Ok(flake) = std::env::var("NIX_SHELL_WRAPPER_FLAKE") {
+        let_exprs.push(format!("  systemFlake = builtins.getFlake {flake:?};"));
+        with_exprs.push(format!(
+            "systemFlake.nix-shell-wrapper-pkgs.{NIX_SHELL_WRAPPER_SYSTEM:?}.default"
+        ));
+    } else {
+        let_exprs.push(format!(r#"  nixpkgs = builtins.getFlake "nixpkgs";"#));
+        let_exprs.push(format!(
+            "  pkgs = import nixpkgs {{ system = {NIX_SHELL_WRAPPER_SYSTEM:?}; }};"
+        ));
+        with_exprs.push(format!("pkgs"));
+    }
+
+    for (name, path) in extra_flakes {
+        let_exprs.push(format!("  {name} = builtins.getFlake {path:?};"));
+    }
+
+    let mut expr = "[".to_string();
+
+    if !with_exprs.is_empty() {
+        expr = format!("with {}; {expr}", with_exprs.join(" "));
+    }
+
+    if !let_exprs.is_empty() {
+        expr = format!("let\n{}in\n{expr}", let_exprs.join("\n"));
+    }
+
+    expr
 }
