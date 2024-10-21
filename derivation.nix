1614e508e725ebcc9fb23e29d8d08a055a40cede
Tethys Svensson <freaken@freaken.dk>
2024-10-21 15:20:32 +0200

    Update dependencies

--- derivation.nix
+++ derivation.nix
@@ -8,6 +8,6 @@ rustPlatform.buildRustPackage {
   pname = "nix-shell-wrapper";
   version = "0.1.0";
   src = nix-gitignore.gitignoreSource [ "*.nix" ] ./.;
-  cargoSha256 = "sha256-lqbuYIq7NAPVMqiAt70YMPNkGF3ogMIXxTKGcT28rRY=";
+  cargoSha256 = "sha256-zJvFTcIkiZ/PL94zM4/FgjToREFHU3bOkQ0rFg3tDFE=";
   NIX_SHELL_WRAPPER_SYSTEM = system;
 }
