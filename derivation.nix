3c0452b573c6723d4a048b7df0e0459548d478ba
Tethys Svensson <freaken@freaken.dk>
2024-11-08 13:12:55 +0100

    Cargo update

--- derivation.nix
+++ derivation.nix
@@ -8,6 +8,6 @@ rustPlatform.buildRustPackage {
   pname = "nix-shell-wrapper";
   version = "0.1.0";
   src = nix-gitignore.gitignoreSource [ "*.nix" ] ./.;
-  cargoSha256 = "sha256-zJvFTcIkiZ/PL94zM4/FgjToREFHU3bOkQ0rFg3tDFE=";
+  cargoSha256 = "sha256-eLhCoFsbX9Oncm+GOWf+X9Cg7a2f6k7EEe35rEPFXmw=";
   NIX_SHELL_WRAPPER_SYSTEM = system;
 }
