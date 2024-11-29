fe9f5cf03f38fbedc0cf887561ef448430175fc0
Kristoffer Søholm <k.soeholm@gmail.com>
2024-11-29 20:07:06 +0100

    Bump to 24.11

--- derivation.nix
+++ derivation.nix
@@ -8,6 +8,6 @@ rustPlatform.buildRustPackage {
   pname = "nix-shell-wrapper";
   version = "0.1.0";
   src = nix-gitignore.gitignoreSource [ "*.nix" ] ./.;
-  cargoSha256 = "sha256-eLhCoFsbX9Oncm+GOWf+X9Cg7a2f6k7EEe35rEPFXmw=";
+  cargoHash = "sha256-eLhCoFsbX9Oncm+GOWf+X9Cg7a2f6k7EEe35rEPFXmw=";
   NIX_SHELL_WRAPPER_SYSTEM = system;
 }
