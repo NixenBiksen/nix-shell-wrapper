4283a502a5a9778d39716b73a8cf048106b82bd2
Tethys Svensson <freaken@freaken.dk>
2025-01-13 09:51:27 +0100

    Update crate

--- derivation.nix
+++ derivation.nix
@@ -8,6 +8,6 @@ rustPlatform.buildRustPackage {
   pname = "nix-shell-wrapper";
   version = "0.1.0";
   src = nix-gitignore.gitignoreSource [ "*.nix" ] ./.;
-  cargoHash = "sha256-fOE0T7p68Ie27rMGDthJ5jXA8B5tYhWntX7x6FFmNEY=";
+  cargoHash = "sha256-aID5CsDLD2ddw2X+Mc/MwaY9OiOJsCtGENcT/cHTYac=";
   NIX_SHELL_WRAPPER_SYSTEM = system;
 }
